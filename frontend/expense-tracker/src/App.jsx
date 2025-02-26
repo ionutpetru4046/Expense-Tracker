4ced5818062ae29553e096a982b195283ed70793
ionutpetru4046 <ionut4046@yahoo.com>
2025-02-26 21:18:11 +0000

    routes added to the project

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,9 +1,42 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
+
 const App = () => {
   return (
-    <div className="text-center text-4xl text-blue-500">
-      App
+    <div>
+      <Router>
+        <Routes>
+          <Route path="" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signup" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/income" exact element={<Expense />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
 
 export default App
+
+const Root = () => {
+  // Check if token exists in the localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  // Redirect the dashboard if authenticated, otherwise redirect to login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
